3063c1418123dd67c0cc0c6647cfd168f15bbafe
Matt Palamos <mattpalamos@hotmail.com>
2017-08-24 21:25:07 -0700

    fixed rendering bug

--- react-client/src/components/InfoPanel.jsx
+++ react-client/src/components/InfoPanel.jsx
@@ -17,6 +17,8 @@ class InfoPanel extends React.Component {
     var role = this.props.role.split(' ').join('').toLowerCase();
 
 
+    var role = this.props.role.split(' ').join('').toLowerCase();
+
     var styles = {
       content: `url(/styles/Resources/${role}.png)`
     }
