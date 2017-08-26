6f5a46de3b7d9efb155177f793fbf7a4848204cf
Matt Palamos <mattpalamos@hotmail.com>
2017-08-26 12:23:39 -0700

    fixed bugs

--- react-client/src/components/InfoPanel.jsx
+++ react-client/src/components/InfoPanel.jsx
@@ -10,24 +10,14 @@ class InfoPanel extends React.Component {
 
 
   render() {
-
-    var split = this.props.role.split('_');
-    var name = split[0];
-
-
-
-
-    var role = this.props.role.split(' ').join('').toLowerCase();
-
-
-
     var split = this.props.role.split('_');
     var name = split[0];
     var role = split[0].split(' ').join('').toLowerCase();
+    var extraInfo;
+
     var styles = {
       content: `url(/styles/Resources/${role}.png)`
     }
-
 
     if (this.props.extraInfo){
       extraInfo = (<Cards extraInfo={this.props.extraInfo}/>);
@@ -35,7 +25,6 @@ class InfoPanel extends React.Component {
     // else {
     //   extraInfo = ();
     // }
-
 
     // if (!this.props.extraInfo) {
     //   var extraInfo = ''
@@ -53,36 +42,6 @@ class InfoPanel extends React.Component {
       </div>
     )
   }
-=======
->>>>>>> fix info panel
-    if (!this.props.extraInfo) {
-        var extraInfo = ''
-      } else {
-        console.log('extra Info: ', this.props.extraInfo);
-        var usernames = this.props.extraInfo.slice(1, this.props.extraInfo.length);
-
-        var extraInfo = this.props.extraInfo[0] + usernames.join(', ');
-        //console.log('usernames: ', usernames);
-      }
-      return (
-      <div id="infoPanel">
-        <h5> {name} </h5>
-        <div className="photos" style={styles}></div>
-        <p> {extraInfo} </p>
-      </div>
-      )}
-<<<<<<< HEAD
-=======
-=======
-    <div id="infoPanel">
-      <div className="photos" style={styles}></div>
-      <p> {name} </p>
-      <Cards extraInfo={this.props.extraInfo}/>
-    </div>
-    )
-  }
->>>>>>> fixed bootstrap issue
 }
->>>>>>> fix info panel
 
 export default InfoPanel;
