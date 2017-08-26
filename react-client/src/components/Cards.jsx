ab6814e640824c5adb96df9ebe013d97fcefba67
gchoupeaux <gchoupeaux@gmail.com>
2017-08-26 14:13:40 -0700

    halfway rebase

--- react-client/src/components/Cards.jsx
+++ react-client/src/components/Cards.jsx
@@ -16,12 +16,53 @@ class Cards extends React.Component {
     Example for Morgana:
     {
       mordredTeam:{mordred:['3','10'], minionofmordred:['3','10'], morgana:['yourself'], oberon:['unknown']}
-      merlinTeam:{merlin:['unknown'], LoyalServant:['unknown'], percival:['unknown']}
+      merlinTeam:{merlin:['unknown'], loyalservant_1:['unknown'], loyalservant_2:['unknown'], , loyalservant_3:['unknown'], percival:['unknown']}
     }
     */
 
+    // loop on each key and bluid table row
 
+    var bad = { 
+      //'mordred':'Mordred', 
+      //'minionofmordred':'Minion of Mordred', 
+      'morgana':'Morgana', 
+      'oberon':'Oberon' 
+    };
     
+    var good = {
+      //'merlin':'Merlin', 
+      //'loyalservant_1':'Loyal Servant', 
+      'percival':'Percival', 
+      'loyalservant_2':'Loyal Servant',
+      'loyalservant_3':'Loyal Servant',
+      'loyalservant_4':'Loyal Servant'
+    };
+  
+    var arr = [[],[]];
+    var table1;
+    var table2;
+      
+    for (var key in merlinTeam){
+      if (key !== 'merlin' && key !== 'loyalservant_1'){
+        arr[1].push([key, good[key] + ' is ' + merlinTeam[key].join(' or ')]);
+      }
+    };
+
+    for (var key in mordredTeam){
+      if (key !== 'mordred' && key !== 'minionofmordred'){
+        arr[0].push([key, bad[key] + ' is ' + mordredTeam[key].join(' or ')]);
+      }
+    };
+
+    table1 = (<tr>
+              <td></td>
+              <td><div className="photosRules" style={{content: `url(/styles/Resources/${arr[1][0]}.png)`}} ></div></td>
+            </tr>);
+
+    table2 = (<tr>
+              <td style={{fontSize:'35px'}}></td>
+              <td style={{fontSize:'35px'}}>{arr[1][1]}</td>
+            </tr>);
     
 
     return (
@@ -42,24 +83,24 @@ class Cards extends React.Component {
           </tr>
           <tr>
             <td style={{fontSize:'35px'}}>Mordred is: {mordredTeam.mordred.join(' or ')}</td>
-            <td style={{fontSize:'35px'}}>Merlin is: {merlinTeam.merlin[0]}</td>
+            <td style={{fontSize:'35px'}}>Merlin is: {mordredTeam.mordred.join(' or ')}</td>
           </tr>
+
           <tr>
             <td><div className="photosRules" style={{content: `url(/styles/Resources/minionofmordred.png)`}} ></div></td>
             <td><div className="photosRules" style={{content: `url(/styles/Resources/loyalservant.png)`}} ></div></td>
           </tr>
           <tr>
-            <td style={{fontSize:'35px'}}>Minion of Mordred is: {mordredTeam.minionofmordred[0]}</td>
-            <td style={{fontSize:'35px'}}>Loyal Servant is: {merlinTeam.loyalservant_1[0]}</td>
+            <td style={{fontSize:'35px'}}>Minion of Mordred is: {mordredTeam.minionofmordred.join(' or ')}</td>
+            <td style={{fontSize:'35px'}}>Loyal Servant is: {merlinTeam.loyalservant_1.join(' or ')}</td>
           </tr>
-          <tr>
-            <td></td>
-            <td><div className="photosRules" style={{content: `url(/styles/Resources/loyalservant_2.png)`}} ></div></td>
-          </tr>
-          <tr>
-            <td style={{fontSize:'35px'}}></td>
-            <td style={{fontSize:'35px'}}>Loyal Servant is: {merlinTeam.loyalservant_2[0]}</td>
-          </tr>
+          
+
+          {table1}  
+          {table2}  
+
+
+
         </tbody>  
       </table>
     </div>
