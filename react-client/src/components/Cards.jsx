780f14a6093f38b94703b158a3a13cf50a4d0884
gchoupeaux <gchoupeaux@gmail.com>
2017-08-26 17:20:40 -0700

    extra info done

--- react-client/src/components/Cards.jsx
+++ react-client/src/components/Cards.jsx
@@ -39,8 +39,8 @@ class Cards extends React.Component {
     };
   
     var arr = [[],[]];
-    var table1;
-    var table2;
+    var table = [];
+    //var table2;
       
     for (var key in merlinTeam){
       if (key !== 'merlin' && key !== 'loyalservant_1'){
@@ -54,15 +54,41 @@ class Cards extends React.Component {
       }
     };
 
-    table1 = (<tr>
-              <td></td>
-              <td><div className="photosRules" style={{content: `url(/styles/Resources/${arr[1][0]}.png)`}} ></div></td>
-            </tr>);
 
-    table2 = (<tr>
-              <td style={{fontSize:'35px'}}></td>
-              <td style={{fontSize:'35px'}}>{arr[1][1]}</td>
-            </tr>);
+    for(var i = 0; i < arr[1].length; i++){
+
+      let image1 = arr[0][i]?`url(/styles/Resources/${arr[0][i][0]}.png)`:'';
+      let image2 = arr[1][i]?`url(/styles/Resources/${arr[1][i][0]}.png)`:'';
+      let name1 = arr[0][i]?arr[0][i][1]:'';
+      let name2 = arr[1][i]?arr[1][i][1]:'';
+
+      let a = <tr>
+                  <td><div className="photosRules" style={{content: image1}} ></div></td>
+                  <td><div className="photosRules" style={{content: image2}} ></div></td>
+                </tr>;
+
+      let b = <tr>
+                  <td style={{fontSize:'35px'}}>{name1}</td>
+                  <td style={{fontSize:'35px'}}>{name2}</td>
+                </tr>;
+
+
+      table.push(a);
+
+      table.push(b);
+
+
+    }
+
+    // table.push(<tr>
+    //             <td></td>
+    //             <td><div className="photosRules" style={{content: `url(/styles/Resources/${arr[1][0][0]}.png)`}} ></div></td>
+    //           </tr>);
+
+    // table.push(<tr>
+    //             <td style={{fontSize:'35px'}}></td>
+    //             <td style={{fontSize:'35px'}}>{arr[1][0][1]}</td>
+    //           </tr>);
     
 
     return (
@@ -82,8 +108,8 @@ class Cards extends React.Component {
             <td><div className="photosRules" style={{content: `url(/styles/Resources/merlin.png)`}} ></div></td>
           </tr>
           <tr>
-            <td style={{fontSize:'35px'}}>Mordred is: {mordredTeam.mordred.join(' or ')}</td>
-            <td style={{fontSize:'35px'}}>Merlin is: {mordredTeam.mordred.join(' or ')}</td>
+            <td style={{fontSize:'35px'}}>Mordred is {mordredTeam.mordred.join(' or ')}</td>
+            <td style={{fontSize:'35px'}}>Merlin is {merlinTeam.merlin.join(' or ')}</td>
           </tr>
 
           <tr>
@@ -91,13 +117,13 @@ class Cards extends React.Component {
             <td><div className="photosRules" style={{content: `url(/styles/Resources/loyalservant.png)`}} ></div></td>
           </tr>
           <tr>
-            <td style={{fontSize:'35px'}}>Minion of Mordred is: {mordredTeam.minionofmordred.join(' or ')}</td>
-            <td style={{fontSize:'35px'}}>Loyal Servant is: {merlinTeam.loyalservant_1.join(' or ')}</td>
+            <td style={{fontSize:'35px'}}>Minion of Mordred is {mordredTeam.minionofmordred.join(' or ')}</td>
+            <td style={{fontSize:'35px'}}>Loyal Servant is {merlinTeam.loyalservant_1.join(' or ')}</td>
           </tr>
           
 
-          {table1}  
-          {table2}  
+          {table.map(element => element)}  
+ 
 
 
 
