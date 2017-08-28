484f5a20aab43788484be57870dd6649fb742e4a
Yessengerey Bolatov <yes.murz@gmail.com>
2017-08-28 01:25:53 -0700

    (ui/ux) Design overhaul

--- react-client/src/components/MissionHistory.jsx
+++ react-client/src/components/MissionHistory.jsx
@@ -7,14 +7,20 @@ class MissionHistory extends React.Component {
   }
 
   render() {
+    let element = '';
+    if (this.props.missionHistory > 0) {
+      element = <ol>
+      {this.props.missionHistory.map((outcome, index)=>{
+        return(<li key={index}> {outcome[0]} </li>)
+      })}
+      </ol>;
+    } else {
+      element = <p>No missions to display yet</p>
+    }
     return (
       <div>
         <h6> Mission History   </h6>
-        <ol>
-        {this.props.missionHistory.map((outcome, index)=>{
-          return(<li key={index}> {outcome[0]} </li>)
-        })}
-        </ol>
+        {element}
       </div>
       )}
 }
